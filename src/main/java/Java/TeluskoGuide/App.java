5cf5ad36acdc4ca662e56c1a6cb31345e95fa52c
Thilakg <thilakg399@gmail.com>
2025-04-03 16:57:19 +0530

    Initial Commit

---
+++ src/main/java/Java/TeluskoGuide/App.java
@@ -0,0 +1,10 @@
+package Java.TeluskoGuide;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! from Telusko guide, java package");
+    }
+}
---
+++ src/main/java/part1/DataTypes.java
@@ -0,0 +1,9 @@
+package part1;
+
+public class DataTypes {
+	public static void main(String args[]) {
+		
+		// Data Types
+	}
+
+}
