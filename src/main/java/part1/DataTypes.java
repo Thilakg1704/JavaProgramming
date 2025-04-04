ead2f310a3e9ca6f99fec8c082a812468ca7c04f
Thilakg <thilakg399@gmail.com>
2025-04-04 12:36:47 +0530

    Type Cast

--- src/main/java/part1/DataTypes.java
+++ src/main/java/part1/DataTypes.java
@@ -5,6 +5,27 @@ public class DataTypes {
 		
 		// Data Types
 		
+		byte b = 127;    //8-bit integer
+		short s = 32000; //16-bit integer
+		int i = 100000;  //32-bit integer
+		long l = 100000000000L; //64-bit integer
+		
+		
+		float f = 10.5f; // 32-bit  floating point
+		double d = 20.99; //64-bit floating point	
+		
+		char c = 'A'; //Character type
+		boolean bool = true; // Boolean  type 
+		
+		
+		System.out.println("Byte: "+b);
+		System.out.println("Short: "+s);
+		System.out.println("Integer: "+i);
+		System.out.println("Long: "+l);
+		System.out.println("Float: "+f);
+		System.out.println("Double: "+d);
+		System.out.println("char: "+c);
+		System.out.println("Boolean: "+bool);
 	}
 
 }
