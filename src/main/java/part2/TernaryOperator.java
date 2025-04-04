7242ca8c6c6eae35ae1427185c677c35c5c32357
Thilakg <thilakg399@gmail.com>
2025-04-04 15:18:21 +0530

    SwitchCase and Ternary Operator

---
+++ src/main/java/part2/TernaryOperator.java
@@ -0,0 +1,11 @@
+package part2;
+
+public class TernaryOperator {
+	public static void main(String args[]) {
+		//Ternary Operator
+		
+		int n =5;
+		String  result = n%2==0? "Even":"odd";
+		System.out.println(result);
+	}
+}
